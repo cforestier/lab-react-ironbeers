02eec9e4e96b8b65b361ec10ab41f93f4afbc254
Raphaël Malet <malet.raphael@gmail.com>
2023-08-08 13:14:36 +0200

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,15 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <Link to="/">
-      <img src="src/logo.svg" alt="Header" />
-    </Link>
+    <div className="header">
+      <Link to="/">
+        <img
+          src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"
+          alt="home"
+          height="100px"
+        />
+      </Link>
+    </div>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
+import allBeers from "../assets/beers.png";
+import randomBeers from "../assets/random-beer.png";
+import newBeer from "../assets/new-beer.png";
 
 function HomePage() {
   return (
     <>
       <h1>Home Page</h1>
       <div className="homepage">
-        <img
-          src={process.env.PUBLIC_URL + "/assets/beers.png"}
-          alt="all beers"
-        />
+        <img src={allBeers} alt="all beers" />
         <Link to="/beers">
           <h2>All Beers</h2>
         </Link>
@@ -18,7 +18,7 @@ function HomePage() {
           laborum ab possimus, quis magni? Earum debitis quidem nesciunt amet
           voluptate veritatis.
         </p>
-        <img src="../assets/random-beer.png" alt="all beers" />
+        <img src={randomBeers} alt="all beers" />
         <Link to="/randombeer">
           <h2>Random Beer</h2>
         </Link>
@@ -28,7 +28,7 @@ function HomePage() {
           laborum ab possimus, quis magni? Earum debitis quidem nesciunt amet
           voluptate veritatis.
         </p>
-        <img src="/assets/new-beer.png" alt="all beers" />
+        <img src={newBeer} alt="all beers" />
         <Link to="/newbeer">
           <h2>New Beer</h2>
         </Link>
